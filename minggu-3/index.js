2c286d466ea8fb4262fa45df2549f28f9b93e818
iamgymie <6257063+gymie@users.noreply.github.com>
2024-10-09 14:31:32 +0700

    contoh minggu 2

---
+++ minggu-3/index.js
@@ -0,0 +1,11 @@
+const mahasiswa = {
+    nama: "budi",
+    umur: 20,
+    email: "budi@microsoft.com",
+    telepon: "08123456789"
+};
+
+// const email = mahasiswa.email;
+const { email, umur } = mahasiswa;
+
+console.log(email, umur);
