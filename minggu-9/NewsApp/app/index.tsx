e758c2fa900a6f6bf145131bb37ae35707939029
iamgymie <6257063+gymie@users.noreply.github.com>
2024-11-28 19:52:23 +0700

    update variabel dan stylesheet

--- minggu-9/NewsApp/app/index.tsx
+++ minggu-9/NewsApp/app/index.tsx
@@ -1,217 +1,138 @@
 import {
-  View,
-  Text,
-  ScrollView,
-  Image
+View,
+Text,
+ScrollView,
+Image,
+StyleSheet
 } from 'react-native'
 
 
 export default function App() {
-  return (
-    <ScrollView>
-      <Text style={{ 
-        fontSize: 24,
-        fontWeight: "bold",
-        marginBottom: 10,
-        textAlign: "center"
-      }}>NewsApp</Text>
-      <ScrollView horizontal
-        style={{ 
-          marginBottom: 10,
-          marginLeft: 10,
-          height: 250
-         }}
-        >
-        <Image source={
-          require("./images/berita.jpg")
-        } style={{
-          width: 300,
-          height: 250,
-          marginRight: 10,
-          borderRadius: 10
-        }} />
-        <Image source={
-          require("./images/berita.jpg")
-        } style={{
-          width: 300,
-          height: 250,
-          marginRight: 10,
-          borderRadius: 10
-        }} />
-        <Image source={
-          require("./images/berita.jpg")
-        } style={{
-          width: 300,
-          height: 250,
-          marginRight: 10,
-          borderRadius: 10
-        }} />
-      </ScrollView>
 
-      <View style={{ 
-        flexDirection: "row",
-        justifyContent: "space-between",
-        margin: 10
-       }}>
-        <Text style={{ 
-          backgroundColor: "blue",
-          padding: 10,
-          width: 100,
-          color: "white",
-          borderRadius: 5,
-          textAlign: "center"
-         }}>Previous</Text>
-        <Text style={{ 
-          backgroundColor: "blue",
-          padding: 10,
-          width: 100,
-          color: "white",
-          borderRadius: 5,
-          textAlign: "center"
-         }}>Next</Text>
-      </View>
+	const berita = [
+		{
+			id: 1,
+			judul: "Judul Berita 1",
+			isi: "Berita 1"
+		},
+		{
+			id: 2,
+			judul: "Judul Berita 2",
+			isi: "berita 2"
+		},
+		{
+			id: 3,
+			judul: "Judul Berita 3",
+			isi: "berita 3"
+		},
+		{
+			id: 4,
+			judul: "Medan Banjir",
+			isi: "Hujan terus, medan jadi banjir 1 meter"
+		}
+	];
 
-      {/* View container berita */}
-      <View style={{ 
-        paddingHorizontal: 10
-       }}>
-        {/* View Berita */}
-        <View style={{ 
-          marginBottom: 10,
-          borderRadius: 10,
-          overflow: "hidden",
-          backgroundColor: "#fff",
-          elevation: 3
-         }}> 
-          <Image
-            style={{ 
-              width: "100%",
-              height: 150
-             }}
-            source={require("./images/berita.jpg")} />
-          <Text style={{ 
-            padding: 10,
-            fontSize: 16,
-            fontWeight: "bold"
-           }}>Judul Berita</Text>
-          <Text
-            style={{ 
-              paddingHorizontal: 10,
-              paddingBottom: 10,
-              fontSize: 14,
-              color: "#666"
-             }}
-           >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae quod, recusandae earum fugit velit voluptatem quo animi at corrupti quae nesciunt eum non culpa, dolorem provident possimus id ipsum eaque.</Text>
-        </View>
-        <View style={{ 
-          marginBottom: 10,
-          borderRadius: 10,
-          overflow: "hidden",
-          backgroundColor: "#fff",
-          elevation: 3
-         }}> 
-          <Image
-            style={{ 
-              width: "100%",
-              height: 150
-             }}
-            source={require("./images/berita.jpg")} />
-          <Text style={{ 
-            padding: 10,
-            fontSize: 16,
-            fontWeight: "bold"
-           }}>Judul Berita</Text>
-          <Text
-            style={{ 
-              paddingHorizontal: 10,
-              paddingBottom: 10,
-              fontSize: 14,
-              color: "#666"
-             }}
-           >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae quod, recusandae earum fugit velit voluptatem quo animi at corrupti quae nesciunt eum non culpa, dolorem provident possimus id ipsum eaque.</Text>
-        </View>
-        <View style={{ 
-          marginBottom: 10,
-          borderRadius: 10,
-          overflow: "hidden",
-          backgroundColor: "#fff",
-          elevation: 3
-         }}> 
-          <Image
-            style={{ 
-              width: "100%",
-              height: 150
-             }}
-            source={require("./images/berita.jpg")} />
-          <Text style={{ 
-            padding: 10,
-            fontSize: 16,
-            fontWeight: "bold"
-           }}>Judul Berita</Text>
-          <Text
-            style={{ 
-              paddingHorizontal: 10,
-              paddingBottom: 10,
-              fontSize: 14,
-              color: "#666"
-             }}
-           >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae quod, recusandae earum fugit velit voluptatem quo animi at corrupti quae nesciunt eum non culpa, dolorem provident possimus id ipsum eaque.</Text>
-        </View>
-        <View style={{ 
-          marginBottom: 10,
-          borderRadius: 10,
-          overflow: "hidden",
-          backgroundColor: "#fff",
-          elevation: 3
-         }}> 
-          <Image
-            style={{ 
-              width: "100%",
-              height: 150
-             }}
-            source={require("./images/berita.jpg")} />
-          <Text style={{ 
-            padding: 10,
-            fontSize: 16,
-            fontWeight: "bold"
-           }}>Judul Berita</Text>
-          <Text
-            style={{ 
-              paddingHorizontal: 10,
-              paddingBottom: 10,
-              fontSize: 14,
-              color: "#666"
-             }}
-           >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae quod, recusandae earum fugit velit voluptatem quo animi at corrupti quae nesciunt eum non culpa, dolorem provident possimus id ipsum eaque.</Text>
-        </View>
-        <View style={{ 
-          marginBottom: 10,
-          borderRadius: 10,
-          overflow: "hidden",
-          backgroundColor: "#fff",
-          elevation: 3
-         }}> 
-          <Image
-            style={{ 
-              width: "100%",
-              height: 150
-             }}
-            source={require("./images/berita.jpg")} />
-          <Text style={{ 
-            padding: 10,
-            fontSize: 16,
-            fontWeight: "bold"
-           }}>Judul Berita</Text>
-          <Text
-            style={{ 
-              paddingHorizontal: 10,
-              paddingBottom: 10,
-              fontSize: 14,
-              color: "#666"
-             }}
-           >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae quod, recusandae earum fugit velit voluptatem quo animi at corrupti quae nesciunt eum non culpa, dolorem provident possimus id ipsum eaque.</Text>
-        </View>
-      </View>
-    </ScrollView>
-  )
+	
+return (
+	<ScrollView>
+		<Text style={styles.judulAplikasi}>NewsApp</Text>
+		<ScrollView horizontal
+			style={styles.containerGambar}
+		>
+			<Image source={
+					require("./images/berita.jpg")
+			} style={styles.gambarHightlight} />
+			<Image source={
+				require("./images/berita.jpg")
+			} style={styles.gambarHightlight} />
+			<Image source={
+				require("./images/berita.jpg")
+			} style={styles.gambarHightlight} />
+		</ScrollView>
+
+		<View style={styles.navigasi}>
+			<Text style={styles.tombol}>Previous</Text>
+			<Text style={styles.tombol}>Next</Text>
+		</View>
+
+		{/* View container berita */}
+		<View style={styles.containerBerita}>
+			{/* View Berita */}
+			{
+					berita.map((item, index) => {
+						return (
+							<View style={styles.artikelBerita}> 
+								<Image
+										style={styles.gambarBerita}
+										source={require("./images/berita.jpg")} />
+								<Text style={styles.judulBerita}>{item.judul}</Text>
+								<Text
+										style={styles.teksBerita}
+								>{item.isi}</Text>
+						</View>
+						)
+					})
+			}
+		</View>
+	</ScrollView>
+)
 }
+
+
+const styles = StyleSheet.create({
+	judulAplikasi: { 
+		fontSize: 24,
+		fontWeight: "bold",
+		marginBottom: 10,
+		textAlign: "center"
+	},
+	gambarHightlight:{
+		width: 300,
+		height: 250,
+		marginRight: 10,
+		borderRadius: 10
+	},
+	containerGambar: { 
+		marginBottom: 10,
+		marginLeft: 10,
+		height: 250
+	},
+	navigasi:{ 
+		flexDirection: "row",
+		justifyContent: "space-between",
+		margin: 10
+	},
+	tombol: { 
+		backgroundColor: "blue",
+		padding: 10,
+		width: 100,
+		color: "white",
+		borderRadius: 5,
+		textAlign: "center"
+	},
+	containerBerita: { 
+		paddingHorizontal: 10
+	},
+	artikelBerita: { 
+		marginBottom: 10,
+		borderRadius: 10,
+		overflow: "hidden",
+		backgroundColor: "#fff",
+		elevation: 3
+	},
+	gambarBerita: { 
+		width: "100%",
+		height: 150
+	},
+	judulBerita: { 
+		padding: 10,
+		fontSize: 16,
+		fontWeight: "bold"
+	},
+	teksBerita: { 
+		paddingHorizontal: 10,
+		paddingBottom: 10,
+		fontSize: 14,
+		color: "#666"
+	}
+});
